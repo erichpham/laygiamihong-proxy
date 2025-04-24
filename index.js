bf56494e78ace01c50eaaf16b857738837da98b9
erichpham <124853137+erichpham@users.noreply.github.com>
2025-04-24 12:23:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const axios = require('axios');
-const https = require('https');  // Cần thêm thư viện https
+const https = require('https');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Cấu hình httpsAgent để bỏ qua kiểm tra chứng chỉ SSL
-const agent = new https.Agent({  
-  rejectUnauthorized: false  // Bỏ qua xác thực SSL
+const agent = new https.Agent({
+  rejectUnauthorized: false
 });
 
 app.get('/mihong', async (req, res) => {
   try {
+    console.log('Fetching data from Mi Hồng...');
+    
+    // Thực hiện yêu cầu đến Mi Hồng
     const response = await axios.get('https://www.mihong.vn/api/v1/gold/prices/current', {
       httpsAgent: agent  // Gửi yêu cầu với agent không xác thực SSL
     });
 
-    // Trả dữ liệu về cho client
+    console.log('Data fetched successfully.');
     res.json(response.data);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch data from Mi Hong' });
+    console.error('Error occurred while fetching data from Mi Hồng:', error.message);
+    res.status(500).json({ error: 'Failed to fetch data from Mi Hồng', details: error.message });
   }
 });
 
