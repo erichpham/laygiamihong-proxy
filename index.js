350a67a97f1a02ae7a4f1f06f7e11eeaf102a46b
erichpham <124853137+erichpham@users.noreply.github.com>
2025-04-24 12:32:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
-
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-// Enable CORS (Cross-Origin Resource Sharing) if you are using this API in a browser
-app.use(cors());
-
-// Route to fetch gold price data from Mihong API
+// Định nghĩa endpoint '/gold-prices'
 app.get('/gold-prices', async (req, res) => {
-    try {
-        const response = await axios.get('https://www.mihong.vn/api/v1/gold/prices/current', {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        res.json(response.data); // Return the data as JSON
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Failed to fetch data from Mihong API' });
-    }
+  try {
+    const response = await axios.get('https://www.mihong.vn/api/v1/gold/prices/current');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
